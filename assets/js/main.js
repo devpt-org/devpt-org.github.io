2560cc3d4c129d66f219549de84deb37b5590f3b
Pedro Cruz <pedropcruz@outlook.pt>
2021-06-16 09:55:49 +0100

    fix(eslint): rules on js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 function useHandleScroll() {
-  const navElement = document.querySelector(".ui-nav");
+  const navElement = document.querySelector('.ui-nav');
 
   if (window.pageYOffset > 50) {
-    navElement.classList.add("ui-nav__shadow");
+    navElement.classList.add('ui-nav__shadow');
   } else {
-    navElement.classList.remove("ui-nav__shadow");
+    navElement.classList.remove('ui-nav__shadow');
   }
 }
 
@@ -14,29 +14,30 @@ function useToggleDarkMode() {
 }
 
 function useDarkMode() {
-  document.querySelector("html").classList.add("dark");
-  localStorage.setItem("theme", "dark");
+  document.querySelector('html').classList.add('dark');
+  localStorage.setItem('theme', 'dark');
 }
 
 function useLightMode() {
-  document.querySelector("html").classList.remove("dark");
-  localStorage.setItem("theme", "light");
+  document.querySelector('html').classList.remove('dark');
+  localStorage.setItem('theme', 'light');
 }
 
 function isDarkMode() {
-  const theme = localStorage.getItem("theme");
+  const theme = localStorage.getItem('theme');
 
-  return theme === "dark";
+  return theme === 'dark';
 }
 
-window.addEventListener("load", function () {
-  window.addEventListener("scroll", useHandleScroll);
+window.addEventListener('load', function () {
+  window.addEventListener('scroll', useHandleScroll);
 });
 
 function capitalize(word) {
   return word && word[0].toUpperCase() + word.slice(1);
 }
 
+// eslint-disable-next-line no-unused-vars
 function goToURL(url) {
   if (!url) return;
   window.location.href = url;
@@ -45,21 +46,21 @@ function goToURL(url) {
 function decodeURIFromBreadcrumb(anchor, value) {
   let decodeValue = decodeURI(value);
 
-  if (decodeValue.includes("-")) {
-    decodeValue = [...decodeValue.split("-")]
+  if (decodeValue.includes('-')) {
+    decodeValue = [...decodeValue.split('-')]
       .map((word) => capitalize(word))
-      .join(" ");
+      .join(' ');
   }
 
   anchor.innerHTML = `/ ${decodeValue}`;
 }
 
 (function () {
-  const theme = localStorage.getItem("theme");
-  theme === "light" ? useLightMode() : useDarkMode();
+  const theme = localStorage.getItem('theme');
+  theme === 'light' ? useLightMode() : useDarkMode();
 
-  const breadcrumbLinks = document.querySelectorAll("a[data-breadcrumb]");
+  const breadcrumbLinks = document.querySelectorAll('a[data-breadcrumb]');
   [...breadcrumbLinks].forEach((anchor) =>
-    decodeURIFromBreadcrumb(anchor, anchor.getAttribute("data-breadcrumb"))
+    decodeURIFromBreadcrumb(anchor, anchor.getAttribute('data-breadcrumb'))
   );
 })();
